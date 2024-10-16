976b89651316f0c8081ff39d203cc96a171ca234
mbuthia77 <chege.mc@gmail.com>
2024-10-16 17:28:26 +0300

    Added task 6 script

---
+++ task_6.sql
@@ -0,0 +1,7 @@
+USE alx_book_store;
+
+INSERT INTO customer (customer_name, email, address)
+VALUES 
+('Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+('Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+('Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
