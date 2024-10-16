2c99ce752ef7d79f79902358055d1ccf7b9450c9
mbuthia77 <chege.mc@gmail.com>
2024-10-16 17:39:10 +0300

    Added task 1 script

---
+++ MySQLServer.py
@@ -0,0 +1,27 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+def create_database():
+    try:
+        
+        conn = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='your_password'
+        )
+        
+        cursor = conn.cursor()
+        
+        # Create the database
+        cursor.execute("CREATE DATABASE alx_book_store")
+        print("Database 'alx_book_store' created successfully.")
+        
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_DB_CREATE_EXISTS:
+            print("Database already exists.")
+        else:
+            print(f"Error: {err}")
+    finally:
+        if conn.is_connected():
+            cursor.close()
+            conn.close()
