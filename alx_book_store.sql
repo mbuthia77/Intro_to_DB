4f78840229628b747a537a398415a6d4d7834d58
mbuthia77 <chege.mc@gmail.com>
2024-10-16 16:04:54 +0300

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS alx_book_store;
+CREATE DATABASE alx_book_store;
 
 USE alx_book_store;
 
@@ -13,19 +13,21 @@ CREATE TABLE Authors (author_id  INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 author_name VARCHAR(215)
 ); 
 
-Create TABLE Customers (customer_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+CREATE TABLE Customers (customer_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
 customer_name VARCHAR(215),
 email VARCHAR(215),
 address TEXT
 );
 
 CREATE TABLE Orders (order_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-customer_id INT NOT NULL,
-order_date DATE
+customer_id INT, 
+order_date DATE, 
+FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 ); 
 
 CREATE TABLE Order_Details ( orderdetailid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-order_id INT NOT NULL,
+order_id INT,
 book_id INT NOT NULL,
-quantity DOUBLE
+quantity DOUBLE,
+FOREIGN KEY (order_id) REFERENCES Orders(order_id)
 ); 
